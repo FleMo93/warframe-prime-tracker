139338a81eba23bade1d774aa273f6a56b6d0824
Hammer, Florian <florianhammer@hotmail.de>
2020-05-07 11:57:52 +0200

    Fix missing images

--- build.sh
+++ build.sh
@@ -5,12 +5,11 @@ rm -rf dist
 mkdir -p dist
 
 node ./calculateData.js
-
 ./node_modules/.bin/webpack --env.production
 
-cp -vr public/* dist/
-mkdir -p ./public/img/warframe-img
-cp ./warframe-items/data/img/* ./public/img/warframe-img/
+cp -r public/* dist/
+mkdir -p ./dist/img/warframe-img
+cp ./warframe-items/data/img/* ./dist/img/warframe-img/
 
 HASH="$(git rev-parse HEAD)"
 HTML_HASH="<!-- $HASH -->"
@@ -20,11 +19,3 @@ sed -i "1i ${HTML_HASH}" dist/index.html
 for filename in ./dist/*.js; do
   sed -i "1i ${JS_HASH}" "$filename"
 done
-
-echo " "
-echo "dist conent:"
-( cd dist && find . )
-
-echo " "
-echo "warframe-items content:"
-( cd warframe-items && ls )
